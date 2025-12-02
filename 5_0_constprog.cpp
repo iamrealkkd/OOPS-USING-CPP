02d3c7769c374c40584857967d004aa84999c117
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-12-02 18:01:16 +0530

    Program using const keyword.

---
+++ 5_0_constprog.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int age = 18;
+    const float PI = 3.14159;
+
+    cout << "Age: " << age << endl;
+    cout << "Value of PI: " << PI << endl;
+
+    return 0;
+}
