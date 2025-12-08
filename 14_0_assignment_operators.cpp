f56e1bba11d942692b1e857e8e097aadedca8edd
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-12-08 20:08:20 +0530

    14. Program to demonstrate assignment operators.

---
+++ 14_0_assignment_operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+
+    a += 5;
+    a -= 3;
+    a *= 2;
+    a /= 4;
+    a %= 3;
+
+    cout << "Final value of a = " << a << endl;
+
+    return 0;
+}
